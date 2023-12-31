e4e0232836d991a7a769c340f7250cd837a7c0e3
Dennis <dennis.schwartz.bielefeld@gmail.com>
2023-12-31 17:26:31 +0100

    screenshake added

--- Assets/1. Scripts/Game Systems/EventSystem.cs
+++ Assets/1. Scripts/Game Systems/EventSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using TMPro;
 using UnityEngine;
 
@@ -8,10 +9,16 @@ public class EventSystem : MonoBehaviour
     [Header("Settings")] 
     [SerializeField] private Transform coordinatesLeftBottom ;
     [SerializeField] private Transform coordiantesRightTop;
+
+    [Header("ScreenShake")] 
+    [SerializeField] private float _shakeIntensity;
+    [SerializeField] private float _screenShakeDuration;
     
     [Header("Refs")]
     [SerializeField] private ShieldSystems shield;
     [SerializeField] private EnvironmentController _environmentController;
+    [SerializeField] private CinemachineVirtualCamera _camera;
+
     
     public void Attacked()
     {
@@ -25,7 +32,27 @@ public class EventSystem : MonoBehaviour
             float x = Random.Range(coordinatesLeftBottom.position.x, coordiantesRightTop.position.x);
             float y = Random.Range(coordinatesLeftBottom.position.y, coordiantesRightTop.position.y);
             _environmentController.ApplyEffectFrom(new Vector2(x, y));
+            StartScreenShake();
         }
+    }
+
+    private void StartScreenShake(float intensity = -1)
+    { 
+        StopAllCoroutines();
+        StartCoroutine(ScreenShakeCooldown());
+        float shakeIntensity = intensity == -1 ? _shakeIntensity : intensity;
+        _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = shakeIntensity;
+    }
+
+    private void StopScreenShake()
+    {
+        _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+    }
+    
+    private IEnumerator ScreenShakeCooldown()
+    {
+        yield return new WaitForSeconds(_screenShakeDuration);
+        StopScreenShake();
     }
     
 }
