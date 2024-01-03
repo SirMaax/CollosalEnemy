afd36056ff15ea3a2682228f673e90792372a818
Dennis <dennis.schwartz.bielefeld@gmail.com>
2024-01-03 16:40:33 +0100

    Door and oBstacle logic implemented

--- Assets/1. Scripts/TopDown/TopdownEvent.cs
+++ Assets/1. Scripts/TopDown/TopdownEvent.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class TopdownEvent : MonoBehaviour
 {
+    [Header("References")] 
+    [SerializeField] private GameObject _openDoor; 
+    [SerializeField] private GameObject _closeDoor;
+    
+    
     public enum EEvent
     {
-        DoorTrigger
+        OpenDoor,
+        CloseDoor,
+        OpenDoorAndCloseDoor,
     }
     
 
     public void TriggerEvent(EEvent eEvent, bool delteThisAfterwards = false)
     {
-        switch ((int)eEvent)
+        switch (eEvent)
         {
-            case 0:
-                Debug.Log("Yes u did it. You saved the world!");
+            case EEvent.OpenDoor:
+                //OpenDoor
+                _openDoor.SetActive(false);
                 break;        
-            
+            case EEvent.CloseDoor:
+                //OpenDoor
+                _closeDoor.SetActive(true);
+                break;            
+            case EEvent.OpenDoorAndCloseDoor:
+                //OpenDoor
+                _openDoor.SetActive(false);
+                _closeDoor.SetActive(true);
+                break;
         }
         if (delteThisAfterwards) Destroy(gameObject);
 
